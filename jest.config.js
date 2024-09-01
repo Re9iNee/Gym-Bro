862373eab04168a3ab1152a73c80ec42076c06b4
Re9iNee <attarzadeh76@gmail.com>
2024-09-01 20:43:18 +0330

    remove jest completely

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testMatch: ["**/**/*.test.ts"],
-  verbose: true,
-  forceExit: false,
-  setupFilesAfterEnv: ["<rootDir>/singleton.ts"],
-};
