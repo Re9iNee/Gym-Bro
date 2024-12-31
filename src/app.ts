7c725f9dbf9282107c1d3dfd98e5a1872410ad25
Re9iNee <attarzadeh76@gmail.com>
2024-12-31 19:54:16 +0330

    Refactor-Reset Password page

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,11 @@ const bodyParser = require("body-parser");
 import dailyTipRouter from "./routes/daily-tip.route";
 import difficultyRouter from "./routes/difficulty.route";
 import exerciseRouter from "./routes/exercise.route";
+import forgetPasswordRouter from "./routes/forget-password.route";
+import loginRouter from "./routes/login.route";
 import muscleRouter from "./routes/muscle.route";
 import registerRouter from "./routes/register.route";
-import loginRouter from "./routes/login.route";
-import forgetPasswordRouter from "./routes/forget-password.route";
 import resetPasswordRouter from "./routes/reset-password.route";
-import path from "path";
-import prisma from "./database/prisma";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
-import exp from "constants";
 
 const app = express();
 app.use(bodyParser.json());
@@ -28,48 +24,6 @@ app.use("/muscle", muscleRouter);
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
 app.use("/forget-password", forgetPasswordRouter);
-// app.use("/reset-password/", resetPasswordRouter);
-
-// app.use("/public", express.static(path.join(__dirname, "public")));
-
-async function validateResetPasswordToken(req: any, res: any, next: any) {
-  const token = req.query.token;
-
-  if (!token) {
-    return res.status(400).json({ message: "error", error: "Missing token" });
-  }
-
-  try {
-    const user = await prisma.user.findUniqueOrThrow({
-      where: { resetToken: token },
-    });
-
-    if (user.resetTokenExpiry! < new Date()) {
-      return res.status(401).json({ message: "error", error: "Expired token" });
-    }
-
-    // token is valid
-    return next();
-  } catch (e) {
-    console.error(e);
-    if (e instanceof PrismaClientKnownRequestError) {
-      if (e.code === "P2025") {
-        return res
-          .status(401)
-          .json({ message: "error", error: "Invalid token" });
-      }
-    }
-
-    return res.status(403).send("Invalid token.");
-  }
-}
-
-// Route to serve the reset-password HTML file
-app.get("/reset-password", validateResetPasswordToken, (req, res) => {
-  res.sendFile(path.join(__dirname, "public/reset-password/index.html"));
-});
-
-// Middleware to serve static files (e.g., your HTML files)
-// app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/reset-password/", resetPasswordRouter);
 
 export default app;
--- src/routes/reset-password.route.ts
+++ src/routes/reset-password.route.ts
@@ -1,31 +1,50 @@
-import Express, { NextFunction, Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import path from "path";
+import prisma from "../database/prisma";
 import { ResponseType } from "../types/response.type";
-import { fstat, writeFileSync } from "fs";
 
 const router = Router();
 
-type ResetPasswordResponseType = Response<ResponseType<unknown>>;
-const validateToken = (
+type validateResetPasswordResponse = Response<ResponseType<unknown>>;
+async function validateResetPasswordToken(
   req: Request,
-  res: ResetPasswordResponseType,
+  res: validateResetPasswordResponse,
   next: NextFunction
-) => {
-  const token = req.query.token;
-  if (!token && typeof token !== "string") {
+) {
+  const token = req.query.token as string;
+
+  if (!token) {
     return res.status(400).json({ message: "error", error: "Missing token" });
   }
 
-  console.log({ token });
+  try {
+    const user = await prisma.user.findUniqueOrThrow({
+      where: { resetToken: token },
+    });
 
-  next();
-};
+    if (user.resetTokenExpiry! < new Date()) {
+      return res.status(401).json({ message: "error", error: "Expired token" });
+    }
 
-router.get("/", validateToken, (req, res) => {
-  res
-    .status(200)
-    .sendFile(path.join(__dirname, "/public/reset-password/index.html"));
+    // token is valid
+    return next();
+  } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError) {
+      if (e.code === "P2025") {
+        return res
+          .status(401)
+          .json({ message: "error", error: "Invalid token" });
+      }
+    }
+
+    return res.status(500).json({ message: "error", error: "Forbidden" });
+  }
+}
+
+router.get("/", validateResetPasswordToken, (_, res) => {
+  res.sendFile(path.join(__dirname, "../public/reset-password/index.html"));
 });
 
 export default router;
