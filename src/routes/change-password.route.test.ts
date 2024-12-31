54af02c82d0cb224bd306371907e7fe7c4818b8f
Re9iNee <attarzadeh76@gmail.com>
2024-12-31 21:42:30 +0330

    Feature-Change password draft 1

--- src/routes/change-password.route.test.ts
+++ src/routes/change-password.route.test.ts
@@ -2,6 +2,9 @@ import requestFn from "supertest";
 import { describe, expect, it, vi } from "vitest";
 import app from "../app";
 import validateToken from "../middleware/validate-token.middleware";
+import prisma from "../database/__mocks__/prisma";
+
+vi.mock("../database/prisma");
 
 vi.mock("../middleware/validate-token.middleware", () => {
   return { default: vi.fn((req, res, next) => next()) };
@@ -16,5 +19,29 @@ describe("Change Password Route", () => {
       .send({ password: "newPassword" });
 
     expect(validateToken).toHaveBeenCalled();
+  });
+
+  it("should update user password", async () => {
+    const request = {
+      password: "newPassword",
+    };
+    const response = await fetch
+      .post("/change-password/")
+      .query({ token: "validToken" })
+      .send(request);
+
+    expect(response.body).toMatchObject({
+      message: "OK",
+      data: null,
+    });
+    expect(response.status).toBe(200);
+    expect(prisma.user.update).toHaveBeenCalledWith({
+      where: { resetToken: "validToken" },
+      data: {
+        resetToken: null,
+        resetTokenExpiry: null,
+        password: "newPassword",
+      },
+    });
   });
 });
--- src/routes/change-password.route.ts
+++ src/routes/change-password.route.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
 import validateToken from "../middleware/validate-token.middleware";
+import prisma from "../database/prisma";
 
 const router = Router();
 
 router.post("/", validateToken, async (req, res) => {
+  const token = req.query.token as string;
+  const request = await req.body;
+  const password = request.password;
+
+  await prisma.user.update({
+    where: { resetToken: token },
+    data: { resetToken: null, resetTokenExpiry: null, password },
+  });
+
   res.status(200);
   return res.json({ message: "OK", data: null });
 });
